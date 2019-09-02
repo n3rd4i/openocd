adeb86ebcd7a179a49224a7c505a9ac97ca23045
Andrei <n3rd4i@gmail.com>
2019-09-02 19:36:03 +0200

    update to version 20190828

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,14 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'http://sysprogs.com/getfile/552/openocd-20190715.7z'
+$url        = 'http://sysprogs.com/getfile/587/openocd-20190828.7z'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   url           = $url
   softwareName  = 'openocd*'
-  checksum      = 'FF4EC70CD3A4E771003EFB59CB071E4DF130B57F64E4269085E726AB706FF3B6'
+  checksum      = 'B2B155B34B336805AF835729D0CBA9A3997A33F61D2FA12BE573C8A3B783363C'
   checksumType  = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
 
-Install-ChocolateyPath "$toolsDir\OpenOCD-20190715-0.10.0\bin" -PathType 'Machine'
+Install-ChocolateyPath "$toolsDir\OpenOCD-20190828-0.10.0\bin" -PathType 'Machine'
